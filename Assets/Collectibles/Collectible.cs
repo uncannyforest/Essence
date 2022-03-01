a64ebfae38c33b6a183c28b7edf47d5e6494f70c
Uncanny Forest <git@uncannyforest.com>
2022-02-28 21:55:46 -0500

    Animate collectible collection

--- Assets/Collectibles/Collectible.cs
+++ Assets/Collectibles/Collectible.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteSorter))]
 public class Collectible : MonoBehaviour {
     public Material.Type material;
     public int quantity;
+    public float collectAnimationTime = .5f;
+    public float collectAnimationDistance = 1;
+
+    private SpriteSorter spriteSorter;
+    void Start() { spriteSorter = GetComponent<SpriteSorter>(); }
 
     public static Collectible Instantiate(Collectible prefab, Transform parent, Vector2 location, int quantity) {
         Collectible collectible = Instantiate<Collectible>(prefab, location.WithZ(GlobalConfig.I.elevation.collectibles), Quaternion.identity, parent);
@@ -24,8 +30,18 @@ public class Collectible : MonoBehaviour {
 
         if (inventory != null && !inventory.materials[material].IsFull) {
             inventory.Add(material, quantity, GetComponentInChildren<SpriteRenderer>().sprite);
-            Destroy(gameObject);
+            StartCoroutine(CollectAnimation());
         }
+    }
 
+    private IEnumerator CollectAnimation() {
+        float startTime = Time.time;
+        float endTime = startTime + collectAnimationTime;
+        float speed = collectAnimationDistance / collectAnimationTime;
+        while (Time.time < endTime) {
+            spriteSorter.VerticalDisplacement = speed * (Time.time - startTime);
+            yield return null;
+        }
+        Destroy(gameObject);
     }
 }
