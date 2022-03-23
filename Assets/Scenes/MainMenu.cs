8e26c7b6e45df956a19a876a0cddf34e4669092b
Uncanny Forest <git@uncannyforest.com>
2022-03-23 17:26:01 -0400

    Add main menu
    with New Game and nonfunctional Load Game

---
+++ Assets/Scenes/MainMenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void NewGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadGame() {
+
+    }
+}
